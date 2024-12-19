0c9806ef58ce97dab8b7c8f6f6bcd7bb4699dddc
Ivan <145771734+IvanGricev@users.noreply.github.com>
2024-12-19 15:14:30 +0300

    Basic markup and Bootstrap added

--- header.php
+++ header.php
@@ -9,13 +9,28 @@
     <link rel="stylesheet" href="/styles/styles.css">
 </head>
 <body>
-    <header>
-        <div>
-            <nav>
-                <ul>
-                    <li><a href="index.php">Главная</a></li>
-                    <li><a href="services.php">Услуги</a></li>
-                </ul>
+    <header class="bg-dark">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark ">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">БелУслуги</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                            <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                Пользователь
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="#">Регистрация</a></li>
+                                <li><a class="dropdown-item" href="#">Авторизация</a></li>
+                            </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     </header>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php include("header.php"); ?>
 
-    <main>
-        <h1>Hello World</h1>
+    <main class="pt-3">
+        <div class="container">
+          <h1>Hello World</h1>
+        </div>
     </main>
 
 <?php include("footer.php"); ?>
