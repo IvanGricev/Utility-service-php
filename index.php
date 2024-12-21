9a734ba503251f23081c7a32052256eb01ce4cf0
Ivan <145771734+IvanGricev@users.noreply.github.com>
2024-12-21 08:45:16 +0300

    Slight styles fix

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php include("header.php"); ?>
 
-    <main class="pt-3">
-        <div class="container">
-          <h1>Hello World</h1>
-        </div>
+     <main class="main-content pt-3"> 
+      <div class="container">
+        <h1>Hello World</h1>
+      </div>
     </main>
 
 <?php include("footer.php"); ?>
